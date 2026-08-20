d2e7978153aa5f93a6bf1305d11056a1763de0bc
UnleashInternUGD-Graciel <gracielbalucating.hs@gmail.com>
2026-08-20 15:57:03 +0800

    Revert "Merge branch 'main' into graciel"
    This reverts commit 9e12cb80a6b9ba61bcfa05277459b6c90abccb92, reversing
    changes made to c74bd2f6746187b0d643c5498c62f5293d0719e7.

--- Assets/Scripts/test.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class CollisionTest : MonoBehaviour
-{
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("COLLISION WITH: " + collision.gameObject.name);
-    }
-}
